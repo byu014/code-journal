34eac2885788fe941eacf197156266f399779b78
Bailey Yu <bailey.yu@yahoo.com>
2021-10-26 14:53:07 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -30,3 +30,60 @@ window.addEventListener('beforeunload', () => {
   const dataJSON = JSON.stringify(allData);
   localStorage.setItem('data', dataJSON);
 });
+
+function renderEntry(journalEntry) {
+  /*
+  <div class="col-half">
+    <div class="input">
+      <img class="preview-image" src="https://www.bbvaopenmind.com/wp-content/uploads/2015/12/Ada_Lovelace_Chalon_portrait-1-1024x1024-1.jpg" alt="placeholder-image-square">
+    </div>
+  </div>
+  <div class="col-half">
+    <div class="input">
+      <h2>Ada Lovelace</h2>
+      <p>
+        Augusta Ada King, Countness of Lovelace was an English mathematician and writer,
+        chiefly known for her work on Charles Baggages proposed mechanical general-purpose computer.
+      </p>
+      <p>
+        She was the first to recognize that the machine had application beyond pure calculation,
+        and to have published the first algorithm intended to be carried out by such a machine.
+      </p>
+    </div>
+  </div>
+  */
+  const $li = document.createElement('li');
+  $li.classList.add('row');
+
+  const $colHalf1 = document.createElement('div');
+  $colHalf1.classList.add('col-half');
+
+  const $input1 = document.createElement('div');
+  $input1.classList.add('input');
+
+  const $image = document.createElement('img');
+  $image.classList.add('preview-image');
+  $image.setAttribute('src', journalEntry.photoUrl);
+  $image.setAttribute('alt', 'place-holder-image-square');
+
+  const $colHalf2 = document.createElement('div');
+  $colHalf2.classList.add('col-half');
+  const $input2 = document.createElement('div');
+  $input2.classList.add('input');
+
+  const $h2 = document.createElement('h2');
+  $h2.textContent = journalEntry.title;
+
+  const $p = document.createElement('p');
+  $p.textContent = journalEntry.notes;
+
+  $input2.appendChild($h2);
+  $input2.appendChild($p);
+  $colHalf2.appendChild($input2);
+  $input1.appendChild($image);
+  $colHalf1.appendChild($input1);
+  $li.appendChild($colHalf1);
+  $li.appendChild($colHalf2);
+
+  return $li;
+}
